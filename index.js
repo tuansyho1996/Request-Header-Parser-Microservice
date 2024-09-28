a1316ddaa158466f6b044536e419d8726c7a3da8
tuansyho1996 <hosytuan101296@gmail.com>
2024-09-28 11:24:10 +0700

    feat: update exercise project

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.headers['x-forwarded-for'],
+    language: req.headers['accept-language'],
+    software: req.headers['sec-ch-ua-platform']
+  })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
